89d1a6e277dbff93fd4ec4d0a4a28f354feafe51
Florent Perez <floperez1701@gmail.com>
2026-01-11 21:12:43 +0100

    🎨 Refactor code style - Uniformisation des guillemets et indentation

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,12 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/_next/', '/admin/'],
-      },
-    ],
-    sitemap: 'https://lylusio.fr/sitemap.xml',
-    host: 'https://lylusio.fr',
-  };
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+			disallow: ["/api/", "/_next/", "/admin/"],
+		},
+		sitemap: "https://lylusio.fr/sitemap.xml",
+	};
 }
